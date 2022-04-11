a9c4ebe2902e923b685175820acfc4200c391d89
victor Defontaine <vikthor.d@gmail.com>
2022-04-11 14:02:04 +0200

    Add start function to core ns

--- src/epictetus/core.clj
+++ src/epictetus/core.clj
@@ -1,3 +1,10 @@
-(ns epictetus.core)
+(ns epictetus.core
+  (:require [integrant.core :as ig]
+            [clojure.java.io :as io]
+            [epictetus.window :as _]))
 
-(defn start [] true)
+(defn start
+  ([] (start "engine-default.edn"))
+  ([path]
+   (let [config (-> path io/resource slurp ig/read-string)]
+     (ig/init config))))
