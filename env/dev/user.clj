5578ac3ff9a4541a7f46c11a2f2dc8c348314af4
victor Defontaine <vikthor.d@gmail.com>
2022-08-01 11:50:25 +0200

    Use integrant reloaded worflow through repl-y

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -1,12 +1,14 @@
 (ns user
   (:require [integrant.core :as ig]
             [integrant.repl :refer [clear go halt prep init reset reset-all]]
+            [integrant.repl.state :refer [system config]]
+            [puget.printer :as puget]
             [clojure.java.io :as io]
-            [epictetus.window :as epic-w]))
+            [epictetus.core :as _]))
 
-(defonce config (-> "engine-default.edn"
-                   io/resource
-                   slurp
-                   ig/read-string))
-
-(defn run [] (ig/init config))
+;; https://github.com/weavejester/integrant-repl
+;; Provides worflow function (prep) (init) (go) (reset) (halt)
+(integrant.repl/set-prep! #(ig/prep (-> "engine-default.edn"
+                                        io/resource
+                                        slurp
+                                        ig/read-string)))
