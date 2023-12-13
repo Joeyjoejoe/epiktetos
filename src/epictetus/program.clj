57f095d0a8d60158c93dda59d84619beda9a8b70
Joeyjoejoe <vikthor.d@gmail.com>
2023-12-13 23:35:34 +0100

    Decomplexify :shader/programs system implementation

--- src/epictetus/program.clj
+++ src/epictetus/program.clj
@@ -5,6 +5,87 @@
             [epictetus.lang.opengl :as opengl])
   (:import  (org.lwjgl.opengl GL11 GL20 GL45)))
 
+(defonce gl-types
+  {:vec3f {:bytes (* 3 java.lang.Float/BYTES)
+           :type  GL11/GL_FLOAT
+           :count 3}
+
+   :vec3i {:bytes (* 3 java.lang.Float/BYTES)
+           :type  GL11/GL_INT
+           :count 3}
+
+   :vec3b {:bytes (* 3 java.lang.Float/BYTES)
+           :type  GL11/GL_BYTE
+           :count 3}
+
+   :mat4f {:bytes (* 16 java.lang.Float/BYTES)
+           :type  GL11/GL_FLOAT
+           :count 16}})
+
+(defn attrib-offset
+  "Given the index of an attrib in attribs collection,
+  returns the bytes size sum of previous attribs"
+  [attr-layout location]
+  (let [prev-attribs (subvec (apply vector attr-layout) 0 location)]
+    (->> prev-attribs
+         (map #(get-in gl-types [(:type %) :bytes]))
+         (reduce +))))
+
+(defn attribs-compile
+  "attribs are shader attributes metadata : [0 vec3 varname]
+   layout is a single shader/program layout from config file"
+  [attribs layout]
+  (sort-by :location
+           (for [[location ds] attribs]
+             (let [attrib-key (get layout location) ;; EX: :vec3f/coordinates
+                   attrib-name (name attrib-key)
+                   attrib-type (-> attrib-key
+                                   namespace
+                                   keyword)]
+               {:key       attrib-key
+                :name      attrib-name
+                :type      attrib-type
+                :location  location}))))
+
+(defn compile-uniforms
+  [prog-id uniforms]
+  (GL20/glUseProgram prog-id)
+  (let [unif-with-locations (for [[name _ :as unif] uniforms]
+                              (conj unif (GL20/glGetUniformLocation ^Long prog-id ^String name)))]
+    (GL20/glUseProgram 0)
+    (vec unif-with-locations)))
+
+(defn compile-vao
+  "Create or get a VAO (vertex array object)
+
+  layout  [:float/coordinates :float/color]
+  attribs [[0 :vec3] [1 :vec3]]
+
+  Returns a vao map
+
+  {:vao/id 1
+   :vao/layout [:vec3.float/coordinates :vec3.float/color]
+   :vao/stride 64}"
+  [layout attribs]
+
+  (let [vao         (GL45/glCreateVertexArrays)
+        attr-layout (attribs-compile attribs layout)
+        stride      (->> attr-layout
+                         (map #(get-in gl-types [(:type %) :bytes]))
+                         (reduce +))]
+
+    (doseq [{:keys [location type]} attr-layout]
+      (let [offset       (attrib-offset attr-layout location)
+            attrib-type  (get-in gl-types [type :type])
+            attrib-count (get-in gl-types [type :count])]
+        (GL45/glVertexArrayAttribFormat vao location attrib-count attrib-type false offset)
+        (GL45/glEnableVertexArrayAttrib vao location)
+        (GL45/glVertexArrayAttribBinding vao location 0)))
+
+    {:vao/id     vao
+     :vao/layout layout
+     :vao/stride stride}))
+
 (defn compile-pipeline
   "Given a pipeline config:
 
@@ -40,93 +121,6 @@
                (throw (Exception. (str "shader compilation error: " [stage path] (GL20/glGetShaderInfoLog id 1024)))))
              (assoc metadata :shader/ids [id])))))
 
-
-(defonce gl-types
-  {:float GL11/GL_FLOAT
-   :int   GL11/GL_INT
-   :byte  GL11/GL_BYTE})
-
-(defonce struc-length
-  {:vec3 3
-   :vec4 4
-   :mat3 9
-   :mat4 16})
-
-(defonce type-bytes-sizes
-  {:float java.lang.Float/BYTES
-   :int   java.lang.Integer/BYTES
-   :byte  java.lang.Byte/BYTES})
-
-(defn attrib-bytes
-  "Returns the bytes size of the given attrib"
-  [struc typ]
-  (let [type-bytes   ((keyword typ) type-bytes-sizes)
-        struc-length ((keyword struc) struc-length)]
-    (* struc-length type-bytes)))
-
-(defn attrib-offset
-  "Given the index of an attrib in attribs collection,
-  returns the bytes size sum of previous attribs"
-  [attr-layout location]
-  (let [prev-attribs (subvec (apply vector attr-layout) 0 location)]
-    (->> prev-attribs
-         (map :size)
-         (reduce +))))
-
-(defn attribs-compile
-  [attribs layout]
-  (sort-by :location
-           (for [[location struc] attribs]
-             (let [attrib (get layout location)
-                   field  (name attrib)
-                   typ    (namespace attrib)
-                   k      (keyword (str (name struc) "." typ) field)]
-               {:location location
-                :typ      (keyword typ)
-                :size     (attrib-bytes struc typ)
-                :length   ((keyword struc) struc-length)
-                :key      k}))))
-
-
-
-(defn compile-vao
-  "Create or get a VAO (vertex array object)
-
-  layout  [:float/coordinates :float/color]
-  attribs [[0 :vec3] [1 :vec3]]
-
-  Returns a vao map
-
-  {:vao/id 1
-   :vao/layout [:vec3.float/coordinates :vec3.float/color]
-   :vao/stride 64}"
-  [layout attribs]
-
-  (let [vao         (GL45/glCreateVertexArrays)
-        attr-layout (attribs-compile attribs layout)
-        stride      (->> attr-layout
-                         (map :size)
-                         (reduce +))]
-
-    (doseq [{:keys [length location typ]} attr-layout]
-      (let [offset (attrib-offset attr-layout location)]
-        (GL45/glVertexArrayAttribFormat vao location length (typ gl-types) false offset)
-        (GL45/glEnableVertexArrayAttrib vao location)
-        (GL45/glVertexArrayAttribBinding vao location 0)))
-
-    {:vao/id     vao
-     :vao/layout (vec (map :key attr-layout))
-     :vao/stride stride}))
-
-(defn compile-uniforms
-  [prog-id uniforms]
-  (GL20/glUseProgram prog-id)
-  (let [unif-with-locations (for [[name _ :as unif] uniforms]
-                              (conj unif (GL20/glGetUniformLocation ^Long prog-id ^String name)))]
-    (GL20/glUseProgram 0)
-    (vec unif-with-locations)))
-
-
 (defmethod ig/init-key
   :shader/programs
   [_ programs]
@@ -154,15 +148,3 @@
        :program  {name {:program/id prog-id
                         :layout     (-> vao :vao/layout vec)
                         :uniforms   (compile-uniforms prog-id uniforms)}}}))))
-
-
-;;
-;;
-;;  :shaders {:vao {[:float/coordinates :float/color] {:vao/id 1
-;;                                                     :vao/layout [:float/coordinates :float/color]
-;;                                                     :vao/stride 64}}
-;;            :program {:default {:program/id 1
-;;                                :layout     [:float/coordinates :float/color]
-;;                                :uniforms   [["model" 1 :mat4]
-;;                                             ["view" 2 :mat4]
-;;                                             ["projection" 3 :mat4]]}}}
