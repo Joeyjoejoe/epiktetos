07b660843b50a25d94520a23c7e08231522b6997
Joeyjoejoe <vikthor.d@gmail.com>
2024-03-14 17:07:24 +0100

    Add core coeffect :get-entity

--- src/epictetus/entity.clj
+++ src/epictetus/entity.clj
@@ -1,5 +1,6 @@
 (ns epictetus.entity
   (:require [epictetus.effect :refer [reg-fx]]
+            [epictetus.coeffect :refer [reg-cofx]]
             [epictetus.state  :as state]))
 
 (defn do-mut
@@ -11,9 +12,15 @@
 
 (reg-fx :entity/set
         (fn mutate-entity! [[id muts]]
-          ;; m is a map of entity ids and a map of data to set
           ;; (assoc fx :entity/set [:hero {:motion [:walk :right]}])
           (let [entity (get @state/entities id)]
             (->> muts
                  (reduce do-mut entity)
                  (swap! state/entities assoc id)))))
+
+(reg-cofx :entity/get
+          (fn get-entity
+            [coeffects & ids]
+                (assoc coeffects :entity
+                                 (select-keys @state/entities ids))))
+
