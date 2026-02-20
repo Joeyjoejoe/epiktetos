19607b05dbec0b86510082729d935bc778c08de3
Joeyjoejoe <vikthor.d@gmail.com>
2026-02-21 00:16:40 +0100

    Remove unused old functions

--- src/epiktetos/registrar.clj
+++ src/epiktetos/registrar.clj
@@ -7,34 +7,13 @@
 (defonce render-state
   (atom {}))
 
-(defn get-vao
-  [layout]
-  (get-in @registry [:vao layout]))
-
-(defn add-vao!
-  [vao]
-  (let [layout (:vao/layout vao)]
-    (swap! registry assoc-in [:vao layout] vao)))
-
-(defn get-prog [k]
-  (get-in @registry [:program k]))
-
-(defn add-program!
-  [prog]
-  (let [k (:name prog)]
-
-    ;; Flag previous program for deletion to free up some memory
-    (when-let [old-prog (get-prog k)]
-      (GL20/glDeleteProgram (:id old-prog)))
-
-    (swap! registry assoc-in [:program k] prog)))
-
-
-
-
-(defn get-vao-v2
-  [hash-k]
-  (get-in @registry [::opengl-registry :vaos hash-k]))
+(defn find-vao-by-layout
+  "Finds a registered VAO by its layout hash"
+  [layout-hash]
+  (->> (get-in @registry [::opengl-registry :vaos])
+       (filter (fn [[_ vao]] (= layout-hash (:layout-hash vao))))
+       first
+       second))
 
 (defn register-vao
   [hash-k vao]
@@ -47,14 +26,6 @@
 (defn get-program
   [program-k]
   (get-in @registry [::opengl-registry :programs program-k]))
-
-(defn find-vao-by-layout
-  "Finds a registered VAO by its layout hash"
-  [layout-hash]
-  (->> (get-in @registry [::opengl-registry :vaos])
-       (filter (fn [[_ vao]] (= layout-hash (:layout-hash vao))))
-       first
-       second))
 
 (defn lookup-resource
   ([resource]
