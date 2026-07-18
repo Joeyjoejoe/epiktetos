8b1d114418b41f8d760ce95a93b60b0ebf199040
Joeyjoejoe <vikthor.d@gmail.com>
2026-07-18 23:58:11 +0200

    Replace stale core test with pure form coverage
    core/start no longer exists; the test now covers the pure fx forms of
    render, delete, dispatch, reg-p and reg-input, and unblocks the kaocha
    suite whose loading it broke.

--- env/test/epiktetos/core_test.clj
+++ env/test/epiktetos/core_test.clj
@@ -1,8 +1,25 @@
 (ns epiktetos.core-test
   (:require [clojure.test :as t]
-            [epiktetos.core :as core]))
+            [epiktetos.core :as core]
+            [epiktetos.effect :as fx]))
 
-(t/deftest start
-  (t/testing "Testing start function"
-    (t/is (= true (core/start)) "woo")
-    (t/is (boolean? (core/start)))) "Output is boolean ?")
+(t/deftest pure-forms-test
+  (t/testing "pure forms accumulate their entries in the fx map"
+    (let [handler (fn [_db _step] {})
+          fx      (-> {}
+                      (core/render :triangle {:program :flat})
+                      (core/render :square {:program :flat})
+                      (core/delete :circle)
+                      (core/dispatch :player/damage 10)
+                      (core/reg-p :flat {:pipeline []})
+                      (core/reg-input "Camera" handler {}))]
+      (t/is (= #{[:triangle {:program :flat}] [:square {:program :flat}]}
+               (set (::fx/render fx))))
+      (t/is (= [:circle] (vec (::fx/delete fx))))
+      (t/is (= [[:player/damage 10]] (vec (::fx/dispatch fx))))
+      (t/is (= [[:flat {:pipeline []}]] (vec (::fx/reg-p fx))))
+      (t/is (= [["Camera" handler {}]] (vec (::fx/reg-input fx))))))
+
+  (t/testing "pure forms preserve unrelated fx entries"
+    (let [fx (core/render {:db {:score 1}} :triangle {:program :flat})]
+      (t/is (= {:score 1} (:db fx))))))
